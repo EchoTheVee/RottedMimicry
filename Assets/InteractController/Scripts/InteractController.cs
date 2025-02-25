4db579c66c48e3cd9fc1da12bf5f68df21619a8d
EchoTheVee <taikorodon@gmail.com>
2025-02-25 09:31:13 -0800

    Fixed Dual Door Teleportation Bug

--- Assets/InteractController/Scripts/InteractController.cs
+++ Assets/InteractController/Scripts/InteractController.cs
@@ -6,6 +6,7 @@ public class InteractController : MonoBehaviour
 {
     public GameObject interactIcon;
     public bool isInteractedWith;
+    public bool pIS;
 
     // Start is called before the first frame update
     void Start()
@@ -16,20 +17,34 @@ public class InteractController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (interactIcon.activeInHierarchy && Input.GetKeyDown(KeyCode.E))
+
+        if (interactIcon.activeInHierarchy && Input.GetKeyDown(KeyCode.E) && !isInteractedWith && pIS)
         {
             Interaction();
             isInteractedWith = true;
+            pIS = false;
+            StartCoroutine(InteractionCooldown());
         }
 
-       if (Input.GetKeyUp(KeyCode.E))
-        {
-            isInteractedWith = false;
-        }
     }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+        Debug.Log($"I've been touched by {collision.gameObject.name}");
+        pIS = true;
+    }
+
+
 
     public void Interaction()
     {
-        Debug.Log("YOU INTERACTED WITH ME!");
+        Debug.Log($"YOU INTERACTED WITH {gameObject.name}");
+    }
+
+    IEnumerator InteractionCooldown()
+    {
+        yield return new WaitForSeconds(2);
+        isInteractedWith = false;
     }
 }
